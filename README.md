ac62108b4f7b6132ab10ae600d79c77fb826421c
liuyuxi520 <2805242676@qq.com>
2018-04-26 18:11:54 +0800

    add new fn about draw canvas

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # projectES6
+#安装环境
+#cnpm install gulp-babel --save-dev （gulp-babel 依赖babel-core、babel-preset-es2015）
+#cnpm install babel-core --save-dev
+#cnpm install babel-preset-es2015 --save-dev
+
+#gulpfile.js使用方式
+#var babel = require('gulp-babel');
+<!-- gulp.task("js",function(){
+    gulp.src("./js path")
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+		.pipe(gulp.dest("./js path"))
+}); -->
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),//混淆js
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
+
+gulp.task("js",function(){
+    gulp.src("./public/ES6/**/*.js")
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        // .pipe(uglify({mangle: true,compress: true}))
+		.pipe(gulp.dest("./dist/public/js"))
+        .pipe(reload({stream: true}))
+});
+
+gulp.task("templates", function() {//编译jade成html
+ var YOUR_LOCALS = {};
+ gulp.src("./views/**/*.html")
+   .pipe(gulp.dest("./dist/"))
+});
+
+//启动热更新  
+gulp.task('serve', function() {
+
+    var files = [
+        'views/**/*.html',
+        'public/ES6/../*.js'
+     ]; 
+    browserSync.init({
+        port: 9999,
+        server: {  
+            baseDir: './dist'
+        },
+        files: files,
+    }); 
+   
+}); 
+gulp.task('watch',function(){//监听事件
+    gulp.watch(["./public/ES6/**/*.js"],["js"]);
+    gulp.watch(["./views/**/*.html"],["templates"]); 
+});
+gulp.task("lyx",["js","templates","watch","serve"]);
+gulp.task("default",["lyx"]);
