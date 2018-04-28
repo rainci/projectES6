17eaba9a746807f9dc0fe7520635ea9cf9161042
liuyuxi520 <2805242676@qq.com>
2018-04-28 11:11:05 +0800

    add request 功能

--- public/ES6/request.js
+++ public/ES6/request.js
@@ -1,14 +1,16 @@
 // var axios = require('axios');
-import axios from 'axios';
+// import axios from 'axios';
 axios.get('https://api.btime.com/living/getinfo?&callback=jQuery111308679187464738181_1524815576636&gid=5550c271n388atpio3oiimq6m2j&_=1524815576637')
     .then(res=>{
         // debugger
-        let {data, status, statusText, config} = res;
-        console.log('res:',res)
-        // let pageData = jQuery111308679187464738181_1524815576636(data);
+        let callbackLen = 'jQuery111308679187464738181_1524815576636'.length+1;
+        let newData = res.data.substring(callbackLen);
+            newData = JSON.parse(newData.substring(0,newData.length-1));
+        let {status} = res;
+        let {data} = newData;
         let html;
         let {gid, id, image, title, summary, msg} = data;
-        if(status == 200){
+        if(status == 200 && newData){
             html = `<div>
                 <p>title:${title}</p>
                 <p>msg:${msg}</p>
