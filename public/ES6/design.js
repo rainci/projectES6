c440c99d117007091820a6c691ccfebb011e4ca7
liuyuxi520 <2805242676@qq.com>
2018-05-04 14:28:31 +0800

    modify 代理模式

--- public/ES6/design.js
+++ public/ES6/design.js
@@ -37,26 +37,25 @@ window.addProxy = addProxy;
 window.multProxy2 = multProxy2;
 // window.multProxy = multProxy;
 
-let showImg = (node)=>{
+let showImg =(()=>{
     let imgNode = document.createElement('img');
-    node.appendChild(imgNode);
+    document.body.appendChild(imgNode);
     return {
         setSrc(src){
             imgNode.src = src;
         }
     }
-};
-let showImgProxy = (node) => {
-    let img = new Image();
+})();
+let showImgProxy = (()=>{
+    let img = new Image;
     img.onload = function(){
-        showImg(node).setSrc(this.src);
+        showImg.setSrc(this.src);
     }
     return {
         setSrc(src){
-            showImg(node).setSrc('./load.gif');
+            showImg.setSrc('./load.gif');
             img.src = src;
         }
     }
-};
-showImgProxy(document.getElementById('root'))
-.setSrc('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525966623&di=c1c6d93b1d29af626efd44e2f36a21b2&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.cqqakj.com%2Fupload%2Fimg%2FodSIgNBBloOw3hfPpivZJJDiNdJIt-tBq3Y0VX4CEUVcSZ%2FzODspYVaTJ5LW6i%2F4IveUEQD6mMViUF9zz1luUtx-jHjevL1k.jpg');
+})(); 
+showImgProxy.setSrc('https://static.jintoushou.com/001/170911135031000101.jpeg');
