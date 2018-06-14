c71f41fbee7c579876d0f544c22f7a8423992a00
liuyuxi520 <2805242676@qq.com>
2018-06-14 17:28:33 +0800

    add primise

--- public/ES6/promise.js
+++ public/ES6/promise.js
@@ -271,3 +271,107 @@ let findFn =(()=>{
     }
 })(); 
 console.log('id shi 5:',findFn(arra,5))
+
+
+{
+    async function async1(){
+
+        console.log('async1 start')
+    
+        await async2()
+    
+        console.log('async1 end')
+    
+    }
+    
+    async function async2(){
+    
+        console.log('async2')
+    
+    }
+    
+    console.log('script start')
+    
+    setTimeout(function(){
+    
+        console.log('setTimeout') 
+    
+    },0)  
+    
+    async1();
+    
+    new Promise(function(resolve){
+    
+        console.log('promise1')
+    
+        resolve();
+    
+    }).then(function(){
+    
+        console.log('promise2')
+    
+    })
+    
+    console.log('script end')
+    // 打印顺序
+    // script start
+    // async1 start
+    // async2
+    // promise1
+    // script end
+    // promise2
+    // async1 end
+    // setTimeout
+
+}
+{
+    async function async1(){
+
+        console.log('async1 start')
+    
+        await async2()
+    
+        console.log('async1 end')
+    
+    }
+    
+    function async2(){
+    
+        console.log('async2')
+    
+    }
+    
+    console.log('script start')
+    
+    setTimeout(function(){
+    
+        console.log('setTimeout') 
+    
+    },0)  
+    
+    async1();
+    
+    new Promise(function(resolve){
+    
+        console.log('promise1')
+    
+        resolve();
+    
+    }).then(function(){
+    
+        console.log('promise2')
+    
+    })
+    
+    console.log('script end')
+    // 打印顺序
+    // script start
+    // async1 start
+    // async2
+    // promise1
+    // script end
+    // async1 end
+    // promise2
+    // setTimeout
+
+}
