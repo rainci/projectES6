97c3710c8f2057628d4284f28c05ffb1a224cc76
liuyuxi520 <2805242676@qq.com>
2018-07-09 22:34:10 +0800

    add 模版设计模式

--- public/ES6/design.js
+++ public/ES6/design.js
@@ -129,13 +129,13 @@ Beverage.prototype.boilWater = function(){
     console.log('把水煮沸');
 }
 Beverage.prototype.brew = function(){
-    new Error('子类要重写')
+    throw new Error('子类要重写')
 }
 Beverage.prototype.poulInCup = function(){
-    new Error('子类要重写')
+    throw new Error('子类要重写')
 }
 Beverage.prototype.condition = function(){
-    new Error('子类要重写')
+    throw new Error('子类要重写')
 }
 Beverage.prototype.customer = function(){
     return true;
@@ -151,7 +151,34 @@ Beverage.prototype.init = function(){
 let Coffee = function(){};
 Coffee.prototype = new Beverage();
 Coffee.prototype.brew = function(){
-    console.log('')
+    console.log('用沸水冲咖啡')
+}
+Coffee.prototype.poulInCup = function(){
+    // console.log('把咖啡倒进杯子里')
+}
+Coffee.prototype.condition = function(){
+    console.log('加糖和牛奶')
+}
+Coffee.prototype.customer = function(){
+    return window.confirm('请问加糖和牛奶吗')
+}
+let coff = new Coffee();
+coff.init();
+
+let Tea = function(){};
+Tea.prototype = new Beverage();
+Tea.prototype.brew = function(){
+    console.log('用沸水冲茶')
+}
+Tea.prototype.poulInCup = function(){
+    console.log('把茶倒进杯子里')
+}
+Tea.prototype.condition = function(){
+    console.log('加糖')
+}
+Tea.prototype.customer = function(){
+    return window.confirm('请问加糖吗')
 }
 
-
+let tea = new Tea();
+tea.init();
