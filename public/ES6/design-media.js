aa2931700d1600a6290ae9d0e8b42832726e5886
liufeifeng <liufeifeng@jtoushou.com>
2018-07-17 15:04:04 +0800

    add 中介者d代码

--- public/ES6/design-media.js
+++ public/ES6/design-media.js
@@ -60,6 +60,7 @@ kuncunInput.oninput = function(){
     media.change(this)
 }
 
+// 创建玩家
 function Player(name,color){
     this.name = name;
     this.color = color;
@@ -71,25 +72,92 @@ Player.prototype.win = function(){
 Player.prototype.lose = function(){
     log(`${this.name} lose!`)
 }
+// 玩家死亡
+Player.prototype.dead = function(){
+    this.state = 'dead';
+    // log(`${this.name} dead!`)
+    playDirect.receiveMessage('playerDead',this);
+}
+// 玩家移除
+Player.prototype.remove = function(){
+    playDirect.receiveMessage('removePlayer',this);
+}
+// 玩家换队
+Player.prototype.changeTeam = function(color){
+    playDirect.receiveMessage('changeTeam',this,color);
+}
+var playerFactory = function(name,teamcolor){
+    var newplayer = new Player(name,teamcolor);//创建新玩家
+    playDirect.receiveMessage('addPlayer',newplayer);//给中介者发送消息，增加新玩家
+    return newplayer;
+}
 
-function playDirect(){
+var playDirect = (function(){
     var players = {};//存所有player
     var options = {};//存所有的操作
+    // 增加一个新玩家
     options['addPlayer'] = function(player){
-        var color = palyer.color;
-        palyers[color] = palyers[color] || [];
-        palyers[color].push(player);
-    };
+        var color = player.color; //玩家队伍颜色
+        players[color] = players[color] || []; //如果该颜色的玩具没有成立队伍，则成立新队伍
+        players[color].push(player);//添加新玩家到队伍
+    }; 
+    // 删除一个玩家
     options['removePlayer'] = function(player){
-        var color = palyer.color;
-        palyers[color] = palyers[color] || [];
-        for(var i=palyers[color].length-1,player;playerr=palyers[color][i--];){
-            if(player == playerr){
-                palyers[color].splice(i,1);
+        var color = player.color; //玩家队伍颜色
+        var teamPlayers = players[color] || []; //该颜色的所有成员
+        for(var i=teamPlayers.length-1; i>=0; i--){
+            if(teamPlayers[i] == player){
+                teamPlayers.splice(i,1);
             }
         }       
     };
-    var receiveMessage = function(){
-        options[]
+    // 玩家换队
+    options['changeTeam'] = function(player,newcolor){
+        options['removePlayer'](player);
+        player.color = newcolor;
+        options['addPlayer'](player);
     }
-}
+    // 玩家死亡
+    options['playerDead'] = function(player){
+        var teamColor = player.color, //队伍颜色
+        teamPlayers = players[teamColor]; //玩家队伍
+        var all_dead = true;
+        for(var i=0; i< teamPlayers.length; i++){
+            if(teamPlayers[i].state != 'dead'){
+                all_dead = false;
+                break;
+            }
+        }
+        if(all_dead){//全死亡
+            for(var i=0; i< teamPlayers.length; i++){
+                teamPlayers[i].lose();//本队所有玩家失败
+            }
+            for(var color in players){
+                if(color != teamColor){
+                    var otherplayers = players[color];//其他队伍的玩家
+                    for(var i=0,player;player=otherplayers[i++];){
+                        player.win();
+                    }
+                }
+            }
+        }       
+    }
+
+    var receiveMessage = function(){
+        var opration = Array.prototype.shift.call(arguments);//第一个参数为操作信息
+        options[opration].apply(this,arguments);
+    };
+    return {
+        receiveMessage
+    }
+})();
+var player1 = playerFactory('tiger','red');
+var player2 = playerFactory('hr','red');
+var player3 = playerFactory('xixi','red');
+var player4 = playerFactory('th','blue');
+var player5 = playerFactory('yy','blue');
+var player6 = playerFactory('yx','blue');
+// debugger
+player3.remove();
+player1.dead();
+player2.dead();
