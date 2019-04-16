361332a10804ee3e078e72c591ce14ff866c55a5
univer <univer@univerdeiMac-2.local>
2019-04-16 17:17:15 +0800

    add proxy code

--- public/ES6/proxy.js
+++ public/ES6/proxy.js
@@ -1,4 +1,5 @@
 {
+  //基础示例
   let handler = {
     get(target, prop) {
       return prop in target ? target[prop] : 37;
@@ -18,6 +19,7 @@
 
 }
 {
+  //验证
   let validator = {
     set(obj, prop, value) {
       if (prop === 'age') {
@@ -35,6 +37,38 @@
   let person = new Proxy({}, validator);
   person.age = 100;
   console.log(person.age); // 100
-  person.age = 'young'; // 抛出异常: Uncaught TypeError: The age is not an integer
-  person.age = 300; // 抛出异常: Uncaught RangeError: The age seems invalid
+  // person.age = 'young'; // 抛出异常: Uncaught TypeError: The age is not an integer
+  // person.age = 300; // 抛出异常: Uncaught RangeError: The age seems invalid
+}
+{
+  //值修正及附加属性
+
+  let products = new Proxy({
+    browsers: ['Internet Explorer', 'Netscape']
+  },{
+    get(target,prop){
+      // 附加属性
+      if(prop === 'lastBrowsers'){
+        return target[target.browsers.length-1]
+      }
+      // 缺省行为是返回属性值
+      return target[prop]
+    },
+    set(target,prop,value){
+      if(prop === 'lastBrowsers'){
+        return target['browsers'].push(value)
+      }
+      // 如果不是数组则进行转换
+      if(prop==='browsers' && typeof value === 'string'){
+        value = [value]
+      }
+      // 缺省行为是保存属性值
+      return target[prop] = value
+    }
+  })
+  console.log(1,products.browsers)
+  products.lastBrowsers = 'lyx'
+  console.log(2,products.browsers)
+  products.browsers = 'yang'
+  console.log(2,products.browsers)
 }
